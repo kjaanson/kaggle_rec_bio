dfa0a9e291d08b9df1003c87cf6d047d0b0868ed
Kaur Jaanson <kjaanson@gmail.com>
2021-02-23 14:31:07 +0000

    First commit

---
+++ scripts/train.py
@@ -0,0 +1,223 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+import tensorflow.keras as keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense, Dropout, GlobalAveragePooling2D, Input, concatenate
+from tensorflow.keras.utils import Sequence
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import ModelCheckpoint
+
+from tensorflow.keras.applications import EfficientNetB0
+
+import os, sys, random,copy
+import matplotlib.pyplot as plt
+
+from PIL import Image
+from PIL import ImageFilter
+from sklearn.model_selection import train_test_split
+
+import zipfile
+from io import StringIO, BytesIO
+
+from sklearn.preprocessing import LabelEncoder
+
+from azureml.core import Run
+
+from retry import retry
+
+import argparse
+
+# Helper class for real-time logging
+class CheckpointCallback(keras.callbacks.Callback):
+    def __init__(self, run):
+        self.run = run
+
+    def on_train_begin(self, logs={}):
+        return
+
+    def on_train_end(self, logs={}):
+        return
+
+    def on_epoch_begin(self, epoch, logs={}):
+        return
+
+    def on_epoch_end(self, epoch, logs={}):
+        self.run.log('Training accuracy', logs.get('acc'))
+        self.run.log('Training loss', logs.get('loss'))
+        self.run.log('Validation accuracy', logs.get('val_acc'))
+        self.run.log('Validation loss', logs.get('val_loss'))
+        return
+
+    def on_batch_begin(self, batch, logs={}):
+        return
+
+    def on_batch_end(self, batch, logs={}):
+        return
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data-file', type=str, dest='data_file', help='data folder mounting point')
+    args = parser.parse_args()
+    
+    data_file = args.data_file
+    
+    print("============================================")
+    
+    print("Input dataset: " + data_file)
+    
+    print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+    
+    archive = zipfile.ZipFile(data_file,'r')
+    
+    test_data = pd.read_csv(BytesIO(archive.read('test.csv')))
+    print("Shape of test_data:", test_data.shape)
+    test_data.head()
+    
+    train_data = pd.read_csv(BytesIO(archive.read('train.csv'))).sample(frac=0.25)
+    print("Shape of train_data:", train_data.shape)
+    train_data.head()
+    
+    sirna_label_encoder = LabelEncoder().fit(train_data.sirna)
+    
+    @retry(tries=3)
+    def get_input(experiment, plate, well, site, channel, train=True):
+        if train==True:
+            base_path = 'train'
+        else:
+            base_path = 'test'
+
+        try:
+            path = f"{base_path}/{experiment}/Plate{plate}/{well}_s{str(site)}_w{str(channel)}.png"
+            img = Image.open(BytesIO(archive.read(path)))
+        except KeyError as err:
+            print(f"Error loading input - {err}")
+            print("Will default to other site")
+
+            # hack mis aitab kahe puuduva pildi puhul
+            # pm kui puudub pilt siis proovib lihtsalt teist saiti võtta
+            if site==2:
+                path = f"{base_path}/{experiment}/Plate{plate}/{well}_s1_w{str(channel)}.png"
+                img = Image.open(BytesIO(archive.read(path)))
+            else:
+                path = f"{base_path}/{experiment}/Plate{plate}/{well}_s2_w{str(channel)}.png"
+                img = Image.open(BytesIO(archive.read(path)))
+
+        return(img)
+    
+    class ImgGen(Sequence):
+        def __init__(self, label_data, batch_size = 32, preprocess=(lambda x: x), shuffle=False):
+
+            if shuffle:
+                self.label_data=label_data.sample(frac=1).reset_index(drop=True)
+            else:
+                self.label_data=label_data
+
+            self.batch_size=batch_size
+            self.preprocess=preprocess
+
+        def __len__(self):
+            return int(np.ceil(len(self.label_data))/float(self.batch_size))
+
+        def __getitem__(self, i):
+
+            batch_x = self.label_data.loc[i*self.batch_size:(i+1)*self.batch_size,("experiment","plate","well")]
+            batch_y = self.label_data.loc[i*self.batch_size:(i+1)*self.batch_size,("sirna")]
+
+            x_s1_c1 = [np.array(get_input(e, p, w, site=1, channel=1))/255 for e, p, w in batch_x.values.tolist()]
+            x_s1_c2 = [np.array(get_input(e, p, w, site=1, channel=2))/255 for e, p, w in batch_x.values.tolist()]
+            x_s1_c3 = [np.array(get_input(e, p, w, site=1, channel=3))/255 for e, p, w in batch_x.values.tolist()]
+
+            x_s2_c1 = [np.array(get_input(e, p, w, site=2, channel=1))/255 for e, p, w in batch_x.values.tolist()]
+            x_s2_c2 = [np.array(get_input(e, p, w, site=2, channel=2))/255 for e, p, w in batch_x.values.tolist()]
+            x_s2_c3 = [np.array(get_input(e, p, w, site=2, channel=3))/255 for e, p, w in batch_x.values.tolist()]
+
+            x1 = np.array([x_s1_c1,x_s1_c2,x_s1_c3]).transpose((1,2,3,0))
+            x2 = np.array([x_s2_c1,x_s2_c2,x_s2_c3]).transpose((1,2,3,0))
+
+            y = sirna_label_encoder.transform(batch_y)
+
+            return [np.array(x1), np.array(x2)], y
+        
+        
+    def augment(image):
+        random_transform = random.randint(-1,4)
+        if random_transform==0:
+            image = image.rotate(random.randint(-5,5))
+        if random_transform==1:
+            image = image.filter(ImageFilter.GaussianBlur(radius=1))
+        if random_transform==2:
+            image = image.filter(ImageFilter.RankFilter(size=3, rank=1))
+        if random_transform==3:
+            image = image.filter(ImageFilter.MedianFilter(size=3))
+        if random_transform==4:
+            image = image.filter(ImageFilter.MaxFilter(size=3))
+        return image
+    
+    def create_model():
+        effnet = EfficientNetB0(weights='imagenet',include_top=False,input_shape=(512, 512, 3))
+        site1 = Input(shape=(512,512,3))
+        site2 = Input(shape=(512,512,3))
+        x = effnet(site1)
+        x = GlobalAveragePooling2D()(x)
+        x = Model(inputs=site1, outputs=x)
+        y = effnet(site2)
+        y = GlobalAveragePooling2D()(y)
+        y = Model(inputs=site2, outputs=y)
+        combined = concatenate([x.output, y.output])
+        z = Dropout(0.5)(combined)
+        z = Dense(1108, activation='softmax')(z)
+        model = Model(inputs=[x.input, y.input], outputs=z)
+        model.compile(loss='sparse_categorical_crossentropy', optimizer=Adam(0.0001), metrics=['accuracy'])
+        model.summary()
+
+        return model
+
+    run = Run.get_submitted_run()
+    
+    
+    
+    
+    model = create_model()
+    
+    test_size = 0.025
+    batch_size = 4
+    
+    run.log('Batch Size', batch_size)
+    run.log('Test size', test_size)
+    
+    aml_callback = CheckpointCallback(run)
+    
+    train, val = train_test_split(train_data, test_size=test_size)
+    
+    print(f"Training set size {len(train)}")
+    print(f"Validation set size {len(val)}")
+    
+    train_gen = ImgGen(train,batch_size=batch_size,preprocess=augment,shuffle=True)
+    val_gen = ImgGen(val,batch_size=batch_size,preprocess=augment,shuffle=True)
+    
+    print(f"Training set batched size {len(train_gen)}")
+    print(f"Validation set batched size {len(val_gen)}")
+    
+    os.makedirs('./outputs', exist_ok=True)
+    filepath = './outputs/ModelCheckpoint_all.h5'
+    
+    callback = [
+        ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=True, save_weights_only=False, mode='auto', save_freq='epoch')
+        ]
+    
+    history = model.fit(train_gen, 
+                        steps_per_epoch=len(train)//batch_size, 
+                        epochs=10, 
+                        verbose=1, 
+                        validation_data=val_gen,
+                        validation_steps=len(val)//batch_size,
+                        callbacks=[callback, aml_callback]
+                        )
+    
+    
